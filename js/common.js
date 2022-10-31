0553209175dc57499cd602a5dd4da535ab9e62fe
--abuhosain <cryptichasan925@gmail.com>
2022-10-31 23:46:20 +0600

    add common.js

---
+++ js/common.js
@@ -0,0 +1,17 @@
+
+function updatePhoneNumber(isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    let newPhoneNumber ;
+
+    if(isIncrease === true){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,9 @@
-document.getElementById('btn-phone-plus')
+
+document.getElementById('btn-phone-plus').addEventListener('click',function(){
+    updatePhoneNumber(true)
+})
+
+
+document.getElementById('btn-phone-minus').addEventListener('click',function(){
+    updatePhoneNumber(false)
+})
