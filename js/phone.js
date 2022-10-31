4d641bf8d1605ad42d76c507a4fd09db7d04a760
--abuhosain <cryptichasan925@gmail.com>
2022-10-31 23:57:49 +0600

    semi advance

--- js/phone.js
+++ js/phone.js
@@ -1,9 +1,38 @@
 
+function updatePhoneNumber(isIncrease){
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+    let newPhoneNumber ;
+
+    if(isIncrease === true){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+
+function updatePhoneTotalPrice(newPhoneNumber){
+    const phoneTotalPrize = newPhoneNumber * 1219;
+     const phoneTotalElement = document.getElementById('phone-total');
+     phoneTotalElement.innerText = phoneTotalPrize;
+}
+
 document.getElementById('btn-phone-plus').addEventListener('click',function(){
-    updatePhoneNumber(true)
+     const newPhoneNumber = updatePhoneNumber(true);
+     
+     updatePhoneTotalPrice(newPhoneNumber);
+     
+     
 })
 
 
 document.getElementById('btn-phone-minus').addEventListener('click',function(){
-    updatePhoneNumber(false)
+     const newPhoneNumber = updatePhoneNumber(false);
+
+     updatePhoneTotalPrice(newPhoneNumber);
 })
