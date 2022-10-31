3348dd2c6a2069a9bae331ef7c9cb942cccce410
--abuhosain <cryptichasan925@gmail.com>
2022-10-31 23:28:46 +0600

    module 28-3

---
+++ js/case.js
@@ -0,0 +1,32 @@
+function updateCaseNumber(isIncrease) {
+  const caseNumberField = document.getElementById("case-number-field");
+  const caseNumberString = caseNumberField.value;
+  const previousCaseNumber = parseInt(caseNumberString);
+
+  if (isIncrease === true) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    newCaseNumber = previousCaseNumber - 1;
+  }
+  caseNumberField.value = newCaseNumber;
+  return newCaseNumber;
+}
+
+
+function updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrize = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById("case-total");
+    caseTotalElement.innerText = caseTotalPrize;
+
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  const newCaseNumber = updateCaseNumber(true);
+   updateCaseTotalPrice(newCaseNumber);
+ 
+});
+
+document.getElementById("btn-case-minus").addEventListener("click", function () {
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+  });
---
+++ js/phone.js
@@ -0,0 +1,1 @@
+document.getElementById('btn-phone-plus')
