e9b383f8497785c973f0699554d0887e22b5d41b
--abuhosain <cryptichasan925@gmail.com>
2022-11-01 00:17:56 +0600

    added calculateSubTotal

--- js/phone.js
+++ js/phone.js
@@ -22,10 +22,30 @@ function updatePhoneTotalPrice(newPhoneNumber){
      phoneTotalElement.innerText = phoneTotalPrize;
 }
 
+function getTextElementValueById(elementId){
+    const phoneTotalElement = document.getElementById(elementId);
+    const currentPhoneTotalString = phoneTotalElement.innerText;
+    const currentPhoneTotal = parseInt(currentPhoneTotalString);
+    return currentPhoneTotal;
+}
+
+function calculateSubtotal(){
+     //  calculate total 
+     const currentPhoneTotal = getTextElementValueById('phone-total');
+     const currentCaseTotal =  getTextElementValueById('case-total');
+     const currentSubTotal = currentPhoneTotal + currentCaseTotal;
+     const subTotalElement =  document.getElementById('sub-total');
+     subTotalElement.innerText = currentSubTotal;
+}
+
 document.getElementById('btn-phone-plus').addEventListener('click',function(){
      const newPhoneNumber = updatePhoneNumber(true);
-     
+
      updatePhoneTotalPrice(newPhoneNumber);
+
+     calculateSubtotal();
+
+   
      
      
 })
@@ -35,4 +55,6 @@ document.getElementById('btn-phone-minus').addEventListener('click',function(){
      const newPhoneNumber = updatePhoneNumber(false);
 
      updatePhoneTotalPrice(newPhoneNumber);
+
+     calculateSubtotal();
 })
